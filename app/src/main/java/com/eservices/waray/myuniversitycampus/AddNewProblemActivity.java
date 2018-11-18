d360b31251e764dca87c13489f2fcc6f6cc9322a
waray <mohamedamine.waray@etudiant.univ-lille1.fr>
2018-11-19 00:00:29 +0100

    lat/lng working - geocoder not working

--- app/src/main/java/com/eservices/waray/myuniversitycampus/AddNewProblemActivity.java
+++ app/src/main/java/com/eservices/waray/myuniversitycampus/AddNewProblemActivity.java
@@ -1,7 +1,19 @@
 package com.eservices.waray.myuniversitycampus;
 
+import android.Manifest;
+import android.app.Activity;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationProvider;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,26 +27,46 @@ import android.widget.Toast;
 
 import com.eservices.waray.myuniversitycampus.entity.Problem;
 import com.eservices.waray.myuniversitycampus.model.ProblemViewModel;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
-public class AddNewProblemActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class AddNewProblemActivity extends AppCompatActivity
+        implements AdapterView.OnItemSelectedListener, ActivityCompat.OnRequestPermissionsResultCallback {
 
-    Spinner spinner;
-    Button button;
-    TextView textViewAddress;
-    TextView textViewDescription;
-    Problem.ProblemType problemType;
-    Problem problem;
-    ProblemViewModel problemViewModel;
-    Intent intent;
+    private static final int PERMISSION_REQUEST_LOCATION = 0;
+
+    private Spinner spinner;
+    private Button button;
+    private TextView textViewAddress;
+    private TextView textViewDescription;
+    private Problem.ProblemType problemType;
+    private Problem problem;
+    private ProblemViewModel problemViewModel;
+    private Intent intent;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private double lat,lng;
+    private String address;
+    private View layout;
+    protected Location location;
+    private AddressResultReceiver mResultReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_problem);
-
+        layout = findViewById(R.id.scrollViewAddProblem);
         problemViewModel = ViewModelProviders.of(this).get(ProblemViewModel.class);
+        mResultReceiver = new AddressResultReceiver(new Handler());
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLocation();
+        startIntentService();
 
         intent = new Intent(this,ProblemListActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -66,4 +98,106 @@ public class AddNewProblemActivity extends AppCompatActivity implements AdapterV
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+    private void getLocation(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location loc) {
+                            if (loc != null) {
+                                location = loc;
+                                lat = loc.getLatitude();
+                                lng = loc.getLongitude();
+                                Toast.makeText(getApplicationContext(),"Lat : "+location.getLatitude()+" Long : "+location.getLongitude(),Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        } else {
+            requestLocationPermission();
+        }
+    }
+
+    private void requestLocationPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            Snackbar.make(layout, R.string.location_access_required,
+                    Snackbar.LENGTH_INDEFINITE).setAction(R.string.ok, new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Request the permission here
+                    ActivityCompat.requestPermissions(AddNewProblemActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            PERMISSION_REQUEST_LOCATION);
+                }
+            }).show();
+        } else {
+            Snackbar.make(layout,R.string.location_unavailable, Snackbar.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_REQUEST_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // BEGIN_INCLUDE(onRequestPermissionsResult)
+        if (requestCode == PERMISSION_REQUEST_LOCATION) {
+            // Request for location permission.
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted.
+                getLocation();
+            } else {
+                // Permission request was denied.
+                Snackbar.make(layout, R.string.location_access_denied,
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        }
+        // END_INCLUDE(onRequestPermissionsResult)
+    }
+
+    /**
+     * Creates an intent, adds location data to it as an extra, and starts the intent service for
+     * fetching an address.
+     */
+    private void startIntentService() {
+        // Create an intent for passing to the intent service responsible for fetching the address.
+        Intent intent = new Intent(this, FetchAddressIntentService.class);
+
+        // Pass the result receiver as an extra to the service.
+        intent.putExtra(Constants.RECEIVER, mResultReceiver);
+
+        // Pass the location data as an extra to the service.
+//        intent.putExtra(Constants.LOCATION_DATA_EXTRA, location);
+        intent.putExtra(Constants.LOCATION_LAT, lat);
+        intent.putExtra(Constants.LOCATION_LNG, lng);
+
+        // Start the service. If the service isn't already running, it is instantiated and started
+        // (creating a process for it if needed); if it is running then it remains running. The
+        // service kills itself automatically once all intents are processed.
+        startService(intent);
+    }
+
+    /**
+     * Receiver for data sent from FetchAddressIntentService.
+     */
+    private class AddressResultReceiver extends ResultReceiver {
+        AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        /**
+         *  Receives data sent from FetchAddressIntentService and updates the UI in MainActivity.
+         */
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+
+            // Display the address string or an error message sent from the intent service.
+            address = resultData.getString(Constants.RESULT_DATA_KEY);
+            Toast.makeText(getApplicationContext(),address,Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/eservices/waray/myuniversitycampus/Constants.java
@@ -0,0 +1,17 @@
+package com.eservices.waray.myuniversitycampus;
+
+public final class Constants {
+    public static final int SUCCESS_RESULT = 0;
+    public static final int FAILURE_RESULT = 1;
+    public static final String PACKAGE_NAME =
+            "com.eservices.waray.myuniversitycampus";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME +
+            ".RESULT_DATA_KEY";
+    public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME +
+            ".LOCATION_DATA_EXTRA";
+    public static final String LOCATION_LAT = PACKAGE_NAME +
+            ".LOCATION_LAT";
+    public static final String LOCATION_LNG = PACKAGE_NAME +
+            ".LOCATION_LNG";
+}
